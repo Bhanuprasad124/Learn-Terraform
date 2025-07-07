61cff413d50bf32c8a08d6ee332333475464292c
Bhanuprasad124 <126222548+Bhanuprasad124@users.noreply.github.com>
2025-07-07 07:47:03 +0000

    commit

---
+++ Bascis/terraform.tfvars
@@ -0,0 +1,2 @@
+instance_type = "t2.micro"
+ami="ami-05ffe3c48a9991133"
--- Bascis/variables.tf
+++ Bascis/variables.tf
@@ -1,11 +1,10 @@
 variable "instance_type" {
     description = "type of instance you wnat to use"
-    default = "t2.micro"
+   
   
 }
 
 variable "ami" {
     description = "value of ami"
-    default = "ami-05ffe3c48a9991133"
-  
+
 }
